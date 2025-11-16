f0d61a585fbfb85bba18d364274e2ff6bb896d79
Diego Cuevas <ingdcuevas@gmail.com>
2025-11-15 19:00:00 -0500

    fixing bugs

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,16 +1,16 @@
-import { Event } from "@/database";
+import {Event} from "@/database";
 import connectDB from "../mongodb";
 
 export const getSimilarEventsBySlug = async (slug: string) => {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const event = await Event.findOne({ slug });
-    return await Event.find({
-      _id: { $ne: event._id },
-      tags: { $in: event.tags },
-    }).lean();
-  } catch (error) {
-    return [];
-  }
+        const event = await Event.findOne({slug});
+        return await Event.find({
+            _id: {$ne: event._id},
+            tags: {$in: event.tags},
+        }).lean();
+    } catch (error) {
+        return [];
+    }
 };
